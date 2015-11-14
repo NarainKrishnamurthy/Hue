5b4c3db8750b2d0fddfee69f5cf4988cbb0556e6
Sharangdhar Chakraborty <sharangdharc@yahoo.com>
2015-11-13 21:12:31 -0500

    Initial django setup created

---
+++ webapps/hue/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    #sample urls for app
+
+    #url(r'^$', 'grumblr.views.profile', name='profile'),
+    
+    #url(r'^gs$', 'grumblr.views.gs', name='gs'),
+]
---
+++ webapps/hue/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
+from django.http import HttpResponse
+from django.http import JsonResponse
+from mimetypes import guess_type
+from django.core import serializers
+
+# Decorator to use built-in authentication system
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+
+# Used to create and manually log in a user
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+import json
+
+from grumblr.models import *
+from grumblr.forms import *
