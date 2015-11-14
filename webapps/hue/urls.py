9936fc1fd0c9a05d1591ac68c59e0c50f862f97b
William Yu <williamy@andrew.cmu.edu>
2015-11-14 17:14:10 -0500

    fix urls to reflect changes

--- webapps/hue/urls.py
+++ webapps/hue/urls.py
@@ -2,10 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    #sample urls for app
+  #sample urls for app
 
 	url(r'^$', 'hue.views.home', name='home'),
 	url(r'^home$', 'hue.views.home', name='home'),
-
-  url(r'^test$', 'hue.views.test', name='test'),
 ]
