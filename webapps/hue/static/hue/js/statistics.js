43f8815eba3acede4ee4e609fcef333f0c414ec3
Narain Krishnamurthy <narainsk@cmu.edu>
2015-11-14 22:21:44 -0500

    gotta fix this shit

--- webapps/hue/static/hue/js/statistics.js
+++ webapps/hue/static/hue/js/statistics.js
@@ -2,8 +2,7 @@
 new Chartist.Bar('.ct-chart', {
   labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
   series: [
-    [5, 4, 3, 7, 5, 10, 3],
-    [3, 2, 9, 5, 4, 6, 4]
+    [5, 4, 3, 7, 5, 10, 3]
   ]
 }, {
   seriesBarDistance: 10,
