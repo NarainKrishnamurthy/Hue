18f042597ebf2a3ab078909006888425aaa3752a
Narain Krishnamurthy <narainsk@cmu.edu>
2015-11-14 22:40:40 -0500

    something

--- webapps/hue/static/hue/js/statistics.js
+++ webapps/hue/static/hue/js/statistics.js
@@ -1,5 +1,19 @@
  //var data = {{data | safe}};
-new Chartist.Bar('.ct-chart', {
+new Chartist.Bar('#positive-chart', {
+  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  series: [
+    [5, 4, 3, 7, 5, 10, 3]
+  ]
+}, {
+  seriesBarDistance: 10,
+  reverseData: true,
+  horizontalBars: true,
+  axisY: {
+    offset: 70
+  }
+});
+
+new Chartist.Bar('#negative-chart', {
   labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
   series: [
     [5, 4, 3, 7, 5, 10, 3]
