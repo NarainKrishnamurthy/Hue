adf09b4477c7cc4ccf80ecc341fc6cf3a9dcacf5
William Yu <williamy@andrew.cmu.edu>
2015-11-14 21:25:00 -0500

    revert to stable server

--- webapps/hue/views.py
+++ webapps/hue/views.py
@@ -39,19 +39,19 @@ def home(request):
 		context['search_query'] = search_query
 		context['query'] = True
 
-		twitter.twitter_query(search_query)
-		path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe() ))[0],"datumbox")))
-		file_p = path + '/TextClassification.jar'
-		ifile  = cmd_subfolder + '/data.json'
-		ofile  = path + '/sentiment.csv'
-		os.system("java -jar "+ file_p + ' ' + ifile + ' ' + ofile)
-		path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe() ))[0],"semantic-similarity-master")))
-		cofile = path + '/senti.json'
-		os.system(path + "/similar" + ' ' + ifile + ' ' + ofile + ' ' + cofile)
-		with open(cofile) as data_file:
-			data = json.load(data_file)
-		context['data'] = data
-                print context
+#		twitter.twitter_query(search_query)
+#		path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe() ))[0],"datumbox")))
+#		file_p = path + '/TextClassification.jar'
+#		ifile  = cmd_subfolder + '/data.json'
+#		ofile  = path + '/sentiment.csv'
+#		os.system("java -jar "+ file_p + ' ' + ifile + ' ' + ofile)
+#		path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe() ))[0],"semantic-similarity-master")))
+#		cofile = path + '/senti.json'
+#		os.system(path + "/similar" + ' ' + ifile + ' ' + ofile + ' ' + cofile)
+#		with open(cofile) as data_file:
+#			data = json.load(data_file)
+#		context['data'] = data
+#                print context
 
 	return render(request, 'hue/home.html', context)
 
