89ce389b3f70bb52c43d43b6637433e7c891af05
William Yu <williamy@andrew.cmu.edu>
2015-11-15 09:34:58 -0500

    fixed everything, reddit is weird

--- webapps/hue/static/hue/js/redundant.js
+++ webapps/hue/static/hue/js/redundant.js
@@ -1,6 +1,3 @@
-var d = {{data_tw|safe}};
-console.log(d);
-
 var data_pos = d['comm_pos'];
 var data_neg = d['comm_neg'];
 
@@ -59,3 +56,58 @@ document.getElementById("senti_stat").innerHTML = percentPos + "%";
 var data = {
   series: [percentPos, 100 - percentPos]
 };
+
+var rdata_pos = rd['comm_pos'];
+var rdata_neg = rd['comm_neg'];
+
+var rpmax = data_pos[0]['score'];
+var rnmax = data_neg[0]['score'];
+
+var rpscore_arr = [];
+var rnscore_arr = [];
+
+var rpscore = 0;
+var rnscore = 0;
+
+for(var i=0; i<data_pos.length; i++){
+  var rpstring = 'rpositive';
+  var rnstring = 'rnegative';
+
+  rpscore += ((5 - i) * data_pos[i]['score']);
+  rnscore += ((5 - i) * data_neg[i]['score']);
+
+  rpstring += (i + 1);
+  rnstring += (i + 1);
+
+
+  if (rdata_pos[i]['score'] > rpmax){
+    rpmax = rdata_pos[i]['score'];
+  }
+  if (rdata_neg[i]['score'] > rnmax){
+    rnmax = rdata_neg[i]['score'];
+  }
+
+  rpscore_arr[i] = rdata_pos[i]['score'];
+  rnscore_arr[i] = rdata_neg[i]['score'];
+
+  document.getElementById(rpstring).innerHTML = (i + 1) + "." + rdata_pos[i]['text'];
+  document.getElementById(rnstring).innerHTML = (i + 1) + "." + rdata_neg[i]['text'];      
+}
+var rmain_max = Math.max(rnmax,rpmax);
+for (var j=0;j < rdata_pos.length; j++){
+  rpscore_arr[j] = 10*(rpscore_arr[j] / rmain_max);
+  rnscore_arr[j] = 10*(rnscore_arr[j] / rmain_max);
+}
+rpscore_arr = rpscore_arr.filter(function (x) {return x >= 0;});
+rnscore_arr = rnscore_arr.filter(function (y) {return y >= 0;});
+
+rpos_wrap(rpscore_arr);
+rneg_wrap(rnscore_arr);
+
+var percentPos = Math.floor((pscore / (pscore + nscore))*100);
+
+var p_total = pscore_arr.reduce(function (x, y) {return x+y;});
+var n_total = nscore_arr.reduce(function (x,y) {return x+y;});
+
+percentPos = Math.floor((p_total + n_total == 0) ? 0 : (p_total/(p_total + n_total)) * 100);
+document.getElementById("senti_stat").innerHTML = percentPos + "%";
